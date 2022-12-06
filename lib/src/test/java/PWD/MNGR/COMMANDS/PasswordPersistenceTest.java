6ba4997ee207172be83f19058da033e84f446bb5
denisdbell <denisdbell@gmail.com>
2022-12-06 01:56:43 -0500

    Resolved primary issue test cases

--- lib/src/test/java/PWD/MNGR/COMMANDS/PasswordPersistenceTest.java
+++ lib/src/test/java/PWD/MNGR/COMMANDS/PasswordPersistenceTest.java
@@ -14,52 +14,50 @@ import PWD.MNGR.MODELS.Password;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PasswordPersistenceTest {
-	
-    SessionFactory factory;
 
-   @BeforeAll
-    public void  setup() {
+	SessionFactory factory;
 
-        Configuration configuration = new Configuration()
-                .addAnnotatedClass(Password.class)
-                .setProperty("hibernate.connection.driver_class", "org.sqlite.JDBC")
-                .setProperty("hibernate.connection.url", "jdbc:sqlite:test.sqlite")
-                .setProperty("hibernate.dialect", "org.hibernate.dialect.SQLiteDialect")
-                .setProperty("hibernate.show_sql", "true")
-                .setProperty("hibernate.hdm2ddl.auto", "create-drop");
-        configuration.configure();
+	@BeforeAll
+	public void setup() {
 
-        factory = new Configuration().configure().buildSessionFactory();
-    }
+		Configuration configuration = new Configuration().addAnnotatedClass(Password.class)
+				.setProperty("hibernate.connection.driver_class", "org.sqlite.JDBC")
+				.setProperty("hibernate.connection.url", "jdbc:sqlite:test.db")
+				.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLiteDialect")
+				.setProperty("hibernate.show_sql", "true").setProperty("hibernate.hdm2ddl.auto", "create-drop");
+		configuration.configure();
 
-    @Test
-    public void savePassword() {
-        Password message = new Password("test", "xyz");
-        Session session = factory.openSession();
-        Transaction tx = session.beginTransaction();
-        session.persist(message);
-        tx.commit();
-        session.close();
-    }
+		factory = new Configuration().configure().buildSessionFactory();
+	}
 
-    @Test()
-    public void readMessage() {
-        Session session = factory.openSession();
-        @SuppressWarnings("unchecked")
-        List<Password> list = (List<Password>) session.createQuery("from Message").list();
-        if (list.size() > 1) {
+	@Test
+	public void savePassword() {
+		Password message = new Password("test", "xyz");
+		Session session = factory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.persist(message);
+		tx.commit();
+		session.close();
+	}
+
+	@Test()
+	public void readMessage() {
+		Session session = factory.openSession();
+		@SuppressWarnings("unchecked")
+		List<Password> list = (List<Password>) session.createQuery("from Password").list();
+		if (list.size() > 1) {
 //        	asser
 //            Assert.fail("Message configuration in error; table should contain only one."
 //                    +" Set ddl to create-drop.");
-        }
-        if (list.size() == 0) {
+		}
+		if (list.size() == 0) {
 //            Assert.fail("Read of initial message failed; check saveMessage() for errors."
 //                    +" How did this test run?");
-        }
-        for (Password m : list) {
-            System.out.println(m);
-        }
-        session.close();
-    }
+		}
+		for (Password m : list) {
+			System.out.println(m);
+		}
+		session.close();
+	}
 
 }
